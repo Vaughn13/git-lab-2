482d3486166fb2b05b864673390247f12ecb543a
Vaughn Foster <vf110623@ohio.edu>
2025-01-21 11:42:51 -0500

    update

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Vaughn Foster
+ *        Date: 1/21/25
+ * Description: git lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
